58df8550383ea719aeafae437e9dc9185c914144
datnguyen12032003 <144867683+datnguyen12032003@users.noreply.github.com>
2025-04-14 16:36:36 +0700

    Update UserController.js

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -592,7 +592,7 @@ exports.googleLoginCallback = async (req, res, next) => {
       secure: true, // bắt buộc khi dùng HTTPS
       sameSite: "None", // cho phép gửi cookie cross-origin
     });
-    return res.redirect("https://multi-course-rfc1.vercel.app/course-list");
+    return res.redirect("https://multicourse-client.onrender.com/course-list");
     // res.status(200).json({
     //   message: "Login successful",
     //   token: token,
